f36e7c7b52441dbf28c24a4cd68792c0ec8bb948
root <root@xenodochial-montalcini.213-244-192-215.plesk.page>
2023-08-25 11:53:31 +0200

    yeet

--- entry.cjs
+++ entry.cjs
@@ -1,5 +1,5 @@
 // entry.cjs
 async function loadApp() {
-    await import("./build"); // this is your normal entry file - (index.js, main.js, app.mjs etc.)
+    await import("./build/index.js"); // this is your normal entry file - (index.js, main.js, app.mjs etc.)
 }
 loadApp()
