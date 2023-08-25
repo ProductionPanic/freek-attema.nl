c984305afc935a91d34146bc618ca1b8ef29ec91
Freekiehsoes <freekattema@live.nl>
2023-08-25 11:13:10 +0200

    yes

---
+++ src/lib/components/Pixi.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { beforeUpdate, onDestroy, onMount } from 'svelte';
+	let container: HTMLDivElement;
+
+	onMount(() => {
+		import('./animations').then((module) => {
+			module.default(container);
+		});
+	});
+
+	onDestroy(() => {
+		container && container.dispatchEvent(new Event('destroy'));
+	});
+</script>
+
+<div class="pixi-container" bind:this={container} />
+
+<style lang="scss">
+	.pixi-container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Pixi from '$lib/components/Pixi.svelte';
+</script>
+
+<div class="container">
+	<Pixi />
+</div>
+
+<style lang="scss">
+	.container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
