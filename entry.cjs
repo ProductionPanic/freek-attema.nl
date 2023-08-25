20a098bf9228d7657dbd4feeb6d3ab425251723b
Freekiehsoes <freekattema@live.nl>
2023-08-25 11:49:39 +0200

    test

--- entry.cjs
+++ entry.cjs
@@ -1,5 +1,5 @@
 // entry.cjs
 async function loadApp() {
-    await import("./build/server/index"); // this is your normal entry file - (index.js, main.js, app.mjs etc.)
+    await import("./build"); // this is your normal entry file - (index.js, main.js, app.mjs etc.)
 }
 loadApp()
