3071b10a43445cfa68fec062aab6504cd47a8cb9
root <root@xenodochial-montalcini.213-244-192-215.plesk.page>
2023-08-25 11:29:08 +0200

    fixed entry

--- entry.cjs
+++ entry.cjs
@@ -1,5 +1,5 @@
 // entry.cjs
 async function loadApp() {
-    const { app } = await import("./.svelte-kit/generated/server/internal"); // this is your normal entry file - (index.js, main.js, app.mjs etc.)
+    const { app } = await import("./build/index.js")
 }
 loadApp()
